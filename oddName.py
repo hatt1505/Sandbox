17d6a8582266d8959355411660795d713987caaf
Tran Thu Ha <thuha.tran@my.jcu.edu.au>
2016-11-30 13:42:39 +0800

    ...

--- oddName.py
+++ oddName.py
@@ -1,3 +1,13 @@
 """
 docstring
 """
+# try:
+#     user_input = input("What is your name? ")
+#     for i in range(len(user_input)):
+#         if i%2 == 1:
+#             print (user_input[i])
+#
+# except IndexError:
+#     print
+# print("The second letter of your name is: ", user_input[1])
+">>>"
